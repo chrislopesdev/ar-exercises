37aab1ee584f607061d4771b0c6aa422328b0f1f
Chris Lopes <chris.lopes@gmail.com>
2022-02-01 01:30:10 +0000

    Complete assignment

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -6,6 +6,10 @@ puts "----------"
 # Your code goes below here ...
 class Store < ActiveRecord::Base
   has_many :employees
+
+  validates :name, length: {minimum: 3}
+  validates :annual_revenue, numericality: {:greater_than_or_equal_to => 0}
+
 end
 
 burnaby = Store.create(
--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,27 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+@store1.employees.create(last_name: "test", hourly_rate: 60) # test for first name
+@store1.employees.create(first_name: "test", hourly_rate: 60) # test for last name
+@store1.employees.create(first_name: "test", last_name: "test", hourly_rate: 300) # test max rate
+@store1.employees.create(first_name: "test", last_name: "test", hourly_rate: 10) # test min rate
+b = Store.create( # test store name min characters
+  name: "B",
+  annual_revenue: 300000,
+  mens_apparel: true,
+  womens_apparel: true
+)
+negative = Store.create( # test store with negative revenue
+  name: "Negative",
+  annual_revenue: -10,
+  mens_apparel: true,
+  womens_apparel: true
+)
+
+puts "Give us a store name."
+
+@store_name = gets.chomp
+new_store_name = Store.create( # test store with missing info
+  name: @store_name
+)
+puts new_store_name.errors.messages
